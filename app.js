ff4f9f815c2911f325249a8e8647941ad454708f
DongGiLee <donggi1012@gmail.com>
2023-02-03 14:44:25 +0900

    Init - app.js

--- app.js
+++ app.js
@@ -5,3 +5,29 @@
  * @date 2023-03-03
  * @author donggi
  */
+
+// Express 기본 모듈 불러오기
+const express = require('express')
+    , http = require('http')
+    , path = require('path');
+
+// Express의 미들웨어 불러오기
+const bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , static = require('serve-static')
+    
+
+// 에러핸들러 모듈 사용
+const expressErrorHandler = require('express-error-handler');
+
+// Session 미들웨어 불러오기
+const expressSession = require('express-session');
+
+// 파일 업로드용 미들웨어
+const multer = require('multer');
+const fs = require('fs');
+
+// 클라이언트에서 ajax로 요청시 CORS 지원, (다중서버 접속)
+const cors = require('cors');
+
+let app = express();
